859a4169517bc6d93ae8e16a31d39e69256de2b1
Marc Albert <marcalbert29@outlook.com>
2022-10-03 19:32:04 +0400

    First Commit

---
+++ hello.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+
+    if (filename == "./") {
+        filename = "./index";
+    }
+
+    filename = filename + ".html";
+
+    fs.readFile(filename, function (err, data) {
+        if (err){
+            res.writeHead(404, {'Content-Type': 'text/html'} );
+            return res.end("Error 404 Page Not found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+//    res.writeHead(200, {'Content-Type': 'text/html'});
+    // var q = url.parse(req.url, true).query;
+    // var dates = q.year;
+    // res.write(dates);
+//    res.end();
+}).listen(8080);
+
+console.log("Serer listening on port 8080.....");
